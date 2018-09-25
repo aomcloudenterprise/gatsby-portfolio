b63062e0558c0e320fa53e4a5de0cfbe3317d803
Irene <i.oppo@cantierecreativo.net>
2018-09-25 16:26:43 +0200

    aggiornamento gatsby 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import { StaticQuery } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 
 import '../styles/index.sass'
 
-const TemplateWrapper = ({ children, data }) => (
-  <StaticQuery query={query}>
+const TemplateWrapper = ({ children }) => (
+  <StaticQuery query={graphql`
+    query LayoutQuery
+    {
+      datoCmsSite {
+        globalSeo {
+          siteName
+        }
+        faviconMetaTags {
+          ...GatsbyDatoCmsFaviconMetaTags
+        }
+      }
+      datoCmsHome {
+        seoMetaTags {
+          ...GatsbyDatoCmsSeoMetaTags
+        }
+        introTextNode {
+          childMarkdownRemark {
+            html
+          }
+        }
+        copyright
+      }
+      allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
+        edges {
+          node {
+            profileType
+            url
+          }
+        }
+      }
+    }
+  `}
+  render={data => (
     <div className="container">
       <HelmetDatoCms
         favicon={data.datoCmsSite.faviconMetaTags}
@@ -39,7 +71,7 @@ const TemplateWrapper = ({ children, data }) => (
                 href={profile.url}
                 target="blank"
                 className={`social social--${profile.profileType.toLowerCase()}`}
-              />
+              > </a>
             ))}
           </p>
           <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
@@ -59,84 +91,12 @@ const TemplateWrapper = ({ children, data }) => (
         {children}
       </div>
     </div>
-  </StaticQuery>
+    )}
+  />
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.object,
 }
 
 export default TemplateWrapper
-
-export const query = graphql`
-  query LayoutQuery {
-    datoCmsSite {
-      globalSeo {
-        siteName
-      }
-      faviconMetaTags {
-        ...GatsbyDatoCmsFaviconMetaTags
-      }
-    }
-    datoCmsHome {
-      seoMetaTags {
-        ...GatsbyDatoCmsSeoMetaTags
-      }
-      introTextNode {
-        childMarkdownRemark {
-          html
-        }
-      }
-      copyright
-    }
-    allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
-      edges {
-        node {
-          profileType
-          url
-        }
-      }
-    }
-  }
-`
-
-//
-//
-// export default ({ children, data }) => (
-//   <>
-//     <Helmet titleTemplate={`%s | ${data.site.siteMetadata.title}`} defaultTitle={data.site.siteMetadata.title} />
-//     <div>
-//       {children()}
-//     </div>
-//   </>
-// )
-// export const query = graphql`
-//   query LayoutQuery {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `
-// export default ({ children }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query LayoutQuery {
-//         site {
-//           siteMetadata {
-//             title
-//           }
-//         }
-//       }
-//     `}
-//     render={data => (
-//       <>
-//         <Helmet titleTemplate={`%s | ${data.site.siteMetadata.title}`} defaultTitle={data.site.siteMetadata.title} />
-//         <div>
-//           {children}
-//         </div>
-//       </>
-//     )}
-//   />
-// )
